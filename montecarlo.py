80d34789f3035f6916e00ae3704479b8f9db3784
Rafay <rafay7855@gmail.com>
2018-10-18 18:07:51 -0400

    cleanup and restructure. Working towards 0.1 release

--- montecarlo.py
+++ montecarlo.py
@@ -1,13 +1,56 @@
+#   Monte Carlo simulation of Pi
+#   Rafay Sheikh
+
+#   Simulates value of pi by randomly generating points
+#   on a square grid between (-1, -1) and (1, 1).
+#   Inside the square is an inscribed circle. Any points
+#   with a distance from center <= 1 are inside the circle.
+#   pi / 4 == points inside / points total or 
+
+#   Pi = 4 * (points inside / points total)
+
+##############################################
+
 # import bokeh
 import random as r
 
 import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from math import pi
+import math
 from PyQt5.QtWidgets import * # QApplication, QWidget, QMainWindow, QPushButton
 
 ##############################################
+
+# To be modified by user. 1,000,000 is default
+points_total = 1000000
+
+def main():
+    for x in range(0, points_total):
+        simulation_tick()
+        update_stats()
+        # draw()
+    print_summary()
+
+    
+# These are modified during simluation run
+points_sofar = 0
+points_inside = 0
+
+def simulation_tick():
+    px, py = new_point()
+    d = math.sqrt(px ** 2 + py ** 2) # Distance from center
+    if (d <= 1.0):                   # IF distance is <= 1, point is inside the circle (radius 1)
+        points_inside += 1
+    points_sofar += 1
+
+# Get updated each simulation tick
+experimental_pi = 0.0
+r_squared = 0.0
+
+def update_stats():
+    experimental_pi = 4 * (points_inside / points_sofar)
+    r_squared = 0
 
 
 def new_point():    # All points get random x, y between [-1, 1]
@@ -15,52 +58,43 @@ def new_point():    # All points get random x, y between [-1, 1]
     y = r.uniform(-1, 1)
     return x, y
 
-points_inside = 0
-points_total = 0
 
-experimental_pi = 0.0
-r_squared = 0.0
-def simulation():
-    global points_inside, points_total
+def print_summary():
+    print()
 
-    px, py = new_point()
-    d = px ** 2 + py ** 2 # Distance from center
-    if (d <= 1.0):                      # IF distance is <= 1, point is inside the circle (radius 1)
-        points_inside += 1
-    points_total += 1
-    draw(px, py, d)
-    update_stats(px, py)
-
-     
 def draw(x, y, d):
     pass
 
-def update_stats(px, py):
-    experimental_pi = 4 * (points_inside / points_total)
-    r_squared = 0
+
+
+##############################################
+
+#   Planned: Animate the simulation and make a running graph
+#   of the error between experimental and actual pi
     
-class window(QMainWindow):
-    def __init__(self):
-        super(window, self).__init__()
-        self.init_menu()
-        self.init_simulation()
-        simulation()
+# class window(QMainWindow):
+#     def __init__(self):
+#         super(window, self).__init__()
+#         self.init_menu()
+#         self.init_simulation()
+#         simulation()
 
-    def init_menu(self):
-        mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu('Options')
+#     def init_menu(self):
+#         mainMenu = self.menuBar()
+#         fileMenu = mainMenu.addMenu('Options')
 
-    def init_simulation(self):
-        simview = QHBoxLayout()
-        simview.addStretch(1)
-        # self.setLayout(simview)
+#     def init_simulation(self):
+#         simview = QHBoxLayout()
+#         simview.addStretch(1)
+#         # self.setLayout(simview)
 
-        self.setGeometry(50,50,700,300)
-        self.setWindowTitle('Monte Carlo')
-        self.show()
-def run():
-    app = QApplication(sys.argv)
-    Gui = window()
-    sys.exit(app.exec_())
+#         self.setGeometry(50,50,700,300)
+#         self.setWindowTitle('Monte Carlo')
+#         self.show()
 
-run()
+# def run():
+#     app = QApplication(sys.argv)
+#     Gui = window()
+#     sys.exit(app.exec_())
+
+# run()
