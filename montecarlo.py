fb1ee10cd4457be4352339d35e6a8836d5d2dcab
unknown <rafay7855@gmail.com>
2018-10-19 00:23:07 -0400

    Prelim text version complete. Todo: animating and drawing

--- montecarlo.py
+++ montecarlo.py
@@ -5,7 +5,7 @@
 #   on a square grid between (-1, -1) and (1, 1).
 #   Inside the square is an inscribed circle. Any points
 #   with a distance from center <= 1 are inside the circle.
-#   pi / 4 == points inside / points total or 
+#   pi / 4 == points inside / points total or
 
 #   Pi = 4 * (points inside / points total)
 
@@ -14,11 +14,11 @@
 # import bokeh
 import random as r
 
-import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+#import sys
+#from PyQt5.QtCore import *
+#from PyQt5.QtGui import *
 import math
-from PyQt5.QtWidgets import * # QApplication, QWidget, QMainWindow, QPushButton
+#from PyQt5.QtWidgets import * # QApplication, QWidget, QMainWindow, QPushButton
 
 ##############################################
 
@@ -27,9 +27,9 @@ points_total = 1000000
 
 def main():
     for x in range(0, points_total):
-        simulation_tick()
+        point = simulation_tick()
         update_stats()
-        # draw()
+        draw(point)
     print_summary()
 
     
@@ -38,19 +38,24 @@ points_sofar = 0
 points_inside = 0
 
 def simulation_tick():
-    px, py = new_point()
-    d = math.sqrt(px ** 2 + py ** 2) # Distance from center
+    global points_sofar, points_inside
+    x, y = new_point()
+    d = math.sqrt(x ** 2 + y ** 2) # Distance from center
     if (d <= 1.0):                   # IF distance is <= 1, point is inside the circle (radius 1)
         points_inside += 1
     points_sofar += 1
+    return (x, y, d)
+
 
 # Get updated each simulation tick
 experimental_pi = 0.0
-r_squared = 0.0
+residual = 0
 
 def update_stats():
+    global experimental_pi, residual
     experimental_pi = 4 * (points_inside / points_sofar)
-    r_squared = 0
+    # Difference between observed and expected values
+    residual = experimental_pi - math.pi
 
 
 def new_point():    # All points get random x, y between [-1, 1]
@@ -60,9 +65,17 @@ def new_point():    # All points get random x, y between [-1, 1]
 
 
 def print_summary():
-    print()
+    line = []
+    line.append("---- After {0:d} simulated points ----".format(points_total))
+    line.append("Experimental pi ====> {0:.12f}".format(experimental_pi))
+    line.append("Actual pi       ====> {0:.12f}".format(math.pi))
+    line.append("Residual: {0:.12f}".format(residual))
+    for l in line:
+        print(l, end="\n")
 
-def draw(x, y, d):
+
+def draw(point):
+    x, y, d = point
     pass
 
 
@@ -98,3 +111,10 @@ def draw(x, y, d):
 #     sys.exit(app.exec_())
 
 # run()
+
+import time
+start_time = time.time()
+
+main()
+
+print("---- Completed in {0:.0f} seconds ----".format(time.time() - start_time))
