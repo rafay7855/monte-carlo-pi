91b0410ae355911f722673ac52d95a679c5fd1c5
Rafay <rafay7855@gmail.com>
2019-03-06 20:56:42 -0500

    Switched to logs for reasons to be detailed

--- montecarlo.py
+++ montecarlo.py
@@ -12,28 +12,33 @@
 #   Pi = 4 * (points inside / points total)
 
 ##############################################
-# import bokeh
+
 import random as r
-
-#import sys
-#from PyQt5.QtCore import *
-#from PyQt5.QtGui import *
 import math
-#from PyQt5.QtWidgets import * # QApplication, QWidget, QMainWindow, QPushButton
-
-##############################################
+import time
+import datetime
 
 # To be modified by user. 1,000,000 is default
 points_total = 1000000
 
 def main():
+    start_time = time.time()
+    f_path = "./simulation_logs/"
+    f_date =  datetime.datetime.fromtimestamp(start_time).strftime('%m-%d_%H-%M-%S')
+    filename = f_path + f_date + ".log"
+    
+    logfile = open(filename, "w+")
+    print(filename)
+
     for _ in range(0, points_total):
-        point = simulation_tick()
+        (x, y, d) = simulation_tick()
+        logfile.write("x: {0:.3f} y: {0:.3f} d: {0:.5f}\n".format(x, y, d))
         update_stats()
-        draw(point)
+    global delta_time
+    delta_time = time.time() - start_time
     print_summary()
 
-    
+
 # These are modified during simluation run
 points_sofar = 0
 points_inside = 0
@@ -42,9 +47,14 @@ def simulation_tick():
     global points_sofar, points_inside
     x, y = new_point()
     d = math.sqrt(x ** 2 + y ** 2) # Distance from center
-    if (d <= 1.0):                   # IF distance is <= 1, point is inside the circle (radius 1)
+
+    isInCircle = False
+    if (d <= 1.0): # IF distance is <= 1, point is inside the circle (radius 1)
         points_inside += 1
+        isInCircle = True
     points_sofar += 1
+
+    #print("x: {0:.3f}\ty: {1:.3f}\t d:{2:.4f}\tStatus: {3}\n".format(x, y, d, isInCircle))
     return (x, y, d)
 
 
@@ -64,57 +74,17 @@ def new_point():    # All points get random x, y between [-1, 1]
     y = r.uniform(-1, 1)
     return x, y
 
+delta_time = 0
 
 def print_summary():
-    line = []
-    line.append("---- After {0:d} simulated points ----".format(points_total))
-    line.append("Experimental pi ====> {0:.12f}".format(experimental_pi))
-    line.append("Actual pi       ====> {0:.12f}".format(math.pi))
-    line.append("Residual: {0:.12f}".format(residual))
-    for l in line:  print(l, end="\n")
+    lines = []
+    lines.append("---- After {0:d} simulated points ----".format(points_total))
+    lines.append("Experimental pi ====> {0:.12f}".format(experimental_pi))
+    lines.append("Actual pi       ====> {0:.12f}".format(math.pi))
+    lines.append("Residual: {0:.12f}".format(residual))
+    lines.append("---- Completed in {0:.0f} seconds ----".format(delta_time))
+    for l in lines:  
+        print(l, end="\n")
 
-
-def draw(point):
-    x, y, d = point
-    pass
-
-
-
-##############################################
-
-#   Planned: Animate the simulation and make a running graph
-#   of the error between experimental and actual pi
-    
-# class window(QMainWindow):
-#     def __init__(self):
-#         super(window, self).__init__()
-#         self.init_menu()
-#         self.init_simulation()
-#         simulation()
-
-#     def init_menu(self):
-#         mainMenu = self.menuBar()
-#         fileMenu = mainMenu.addMenu('Options')
-
-#     def init_simulation(self):
-#         simview = QHBoxLayout()
-#         simview.addStretch(1)
-#         # self.setLayout(simview)
-
-#         self.setGeometry(50,50,700,300)
-#         self.setWindowTitle('Monte Carlo')
-#         self.show()
-
-# def run():
-#     app = QApplication(sys.argv)
-#     Gui = window()
-#     sys.exit(app.exec_())
-
-# run()
-
-import time
-start_time = time.time()
 
 main()
-
-print("---- Completed in {0:.0f} seconds ----".format(time.time() - start_time))
